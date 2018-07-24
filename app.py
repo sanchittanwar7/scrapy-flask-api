a616f8fc6968df9899c1533fd7592df7ed10f1ea
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2018-07-24 12:52:03 +0530

    attractions added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 from flask import Flask, request
 from scrapy.crawler import CrawlerRunner
 from tripadvisor import TripAdvisor
+from trip2 import Trip2Spider
 
 app = Flask('Scrape With Flask')
 crawl_runner = CrawlerRunner()      # requires the Twisted reactor to run
@@ -26,6 +27,23 @@ def crawl_for_quotes():
         scrape_in_progress = True
         global quotes_list
         eventual = crawl_runner.crawl(TripAdvisor , location=location, reviews_list=reviews_list)
+        eventual.addCallback(finished_scrape)
+        return 'SCRAPING'
+    elif scrape_complete:
+        return 'SCRAPE COMPLETE'
+    return 'SCRAPE IN PROGRESS'
+
+@app.route('/attractions')
+def crawl_for_attractions():
+    global scrape_in_progress
+    global scrape_complete
+
+    location = request.args.get("location")
+
+    if not scrape_in_progress:
+        scrape_in_progress = True
+        global quotes_list
+        eventual = crawl_runner.crawl(Trip2Spider , location=location, reviews_list=reviews_list)
         eventual.addCallback(finished_scrape)
         return 'SCRAPING'
     elif scrape_complete:
--- trip2.py
+++ trip2.py
@@ -1,19 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import random 
-from scrapy.http.request import Request
 import requests
 
-class TripAdvisor(scrapy.Spider):
-	name = 'tripadvisor'
+
+class Trip2Spider(scrapy.Spider):
+	name = 'trip2'
 	allowed_domains = ['tripadvisor.in']
 
 	def __init__(self, location, reviews_list, *args, **kwargs):
 		self.reviews_list = reviews_list
-		super(TripAdvisor, self).__init__(*args, **kwargs)
+		super(Trip2Spider, self).__init__(*args, **kwargs)
 		self.location = location
 		self.query = ''
-		self.url = "https://www.tripadvisor.in/Hotels-g304552-%sShimla_Shimla_District_Himachal_Pradesh-Hotels.html"
 		self.start_url = "https://www.tripadvisor.com/TypeAheadJson?action=API&types=geonbrhdhoteltheme_park&filter=&legacy_format=true&urlList=true&strictParent=true&query=%s&max=6&name_depth=3&interleaved=true&scoreThreshold=0.5&strictAnd=false&typeahead1_5=true&disableMaxGroupSize=true&geoBoostFix=true&geoPages=&injectList=&neighborhood_geos=true&details=true&rescue=true&uiOrigin=trip_search_Hotels&source=trip_search_Hotels"
 		self.sno = 0
 		self.custom_url = ''
@@ -23,18 +21,20 @@ class TripAdvisor(scrapy.Spider):
 		r = requests.get(url=url)
 		r = r.json()
 		s = r[0]['url'][8:].split('-')
-		dekhio = "https://www.tripadvisor.in/Hotels-" + str(s[1]) + "-%s" + str(s[2]) 
-		url = 'https://www.tripadvisor.in/Hotels-g%s-Shimla_Shimla_District_Himachal_Pradesh-Hotels.html' % r[0]['value']
-		self.url = dekhio
-		yield scrapy.Request(url, callback=self.parse_page)
+		self.url = ("https://www.tripadvisor.in/Attractions-" + str(s[1]) + "-%sActivities-" + str(s[2]))
+		initial_url = ("https://www.tripadvisor.in/Attractions-" + str(s[1]) + "-%sActivities-" + str(s[2])) % ''
+		print(initial_url)
+		yield scrapy.Request(initial_url, callback=self.parse_page)
 
 	def parse_page(self, response):
 		urls = response.css('div.listing_title > a::attr(href)').extract()
 		for url in urls:
 			url = 'https://www.tripadvisor.in' + url
-			self.custom_url = url.split('Reviews')[0] + 'Reviews%s' + url.split('Reviews')[1]
+			try:
+				self.custom_url = url.split('Reviews')[0] + 'Reviews%s' + url.split('Reviews')[1]
+			except (IndexError):
+				continue
 			yield scrapy.Request(url=url, callback=self.parse_reviews)
-
 		# self.sno += 30
 		# self.query = 'oa%s-' % self.sno
 		# url = self.url % (self.query)
@@ -47,14 +47,14 @@ class TripAdvisor(scrapy.Spider):
 		print("------------------------------------------------------")
 		for review in reviews:
 			yield{
-				'hotel_name': response.css('h1#HEADING::text').extract_first(),
-				'user_name' : review.css('div.info_text > div::text').extract_first(),
-				'user_contribution' : review.css('span.badgetext::text').extract_first(),
-				'heading' : review.css('span.noQuotes::text').extract_first(),
-				'review' : review.css('p.partial_entry::text').extract_first()
+			'hotel_name': response.css('h1#HEADING::text').extract_first(),
+			'user_name' : review.css('div.info_text > div::text').extract_first(),
+			'user_contribution' : review.css('span.badgetext::text').extract_first(),
+			'heading' : review.css('span.noQuotes::text').extract_first(),
+			'review' : review.css('p.partial_entry::text').extract_first()
 			}
 			self.reviews_list.append({
-				'hotel_name': response.css('h1#HEADING::text').extract_first(),
+				'place_name': response.css('h1#HEADING::text').extract_first(),
 				'user_name' : review.css('div.info_text > div::text').extract_first(),
 				'user_contribution' : review.css('span.badgetext::text').extract_first(),
 				'heading' : review.css('span.noQuotes::text').extract_first(),
@@ -69,4 +69,9 @@ class TripAdvisor(scrapy.Spider):
 
 
 
-	
+
+
+
+
+
+
